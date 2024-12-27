b431233bc6faab46938601a4e7fb0ab51689be6f
The Zux <thevupro753@gmail.com>
2024-12-27 16:29:12 +0700

    upp

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -6,12 +6,14 @@ import { authenticate } from '@/utils/actions';
 import { useRouter } from 'next/navigation';
 import ModalReactive from './modal';
 import { useState } from 'react';
+import ModalChangePassword from './modal.change.password';
 
 const Login = () => {
 
     const router = useRouter();
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [userEmail, setUserEmail] = useState("");
+    const [changePassword, setChangePassword] = useState(false);
 
     const onFinish = async (values: any) => {
         const { username, password } = values;
@@ -96,9 +98,17 @@ const Login = () => {
 
                             <Form.Item
                             >
-                                <Button type="primary" htmlType="submit">
-                                    Login
-                                </Button>
+                                <div style={{
+                                    display: "flex",
+                                    justifyContent: "space-between",
+                                    alignItems: "center"
+                                }}>
+                                    <Button type="primary" htmlType="submit">
+                                        Login
+                                    </Button>
+                                    <Button type='link' onClick={() => setChangePassword(true)}>Quên mật khẩu ?</Button>
+                                </div>
+
                             </Form.Item>
                             <Form.Item
                             >
@@ -119,6 +129,10 @@ const Login = () => {
                 isModalOpen={isModalOpen}
                 setIsModalOpen={setIsModalOpen}
                 userEmail={userEmail}
+            />
+            <ModalChangePassword
+                isModalOpen={changePassword}
+                setIsModalOpen={setChangePassword}
             />
         </>
 
--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -1,6 +1,8 @@
 'use server'
 
-import { signIn } from "@/auth";
+import { auth, signIn } from "@/auth";
+import { revalidateTag } from 'next/cache'
+import { sendRequest } from "./api";
 
 export async function authenticate(username: string, password: string) {
     try {
@@ -37,4 +39,46 @@ export async function authenticate(username: string, password: string) {
         }
 
     }
+}
+
+export const handleCreateUserAction = async (data: any) => {
+    const session = await auth();
+    const res = await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/users`,
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${session?.user?.access_token}`,
+        },
+        body: { ...data }
+    })
+    revalidateTag("list-users")
+    return res;
+}
+
+export const handleUpdateUserAction = async (data: any) => {
+    const session = await auth();
+    const res = await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/users`,
+        method: "PATCH",
+        headers: {
+            Authorization: `Bearer ${session?.user?.access_token}`,
+        },
+        body: { ...data }
+    })
+    revalidateTag("list-users")
+    return res;
+}
+
+export const handleDeleteUserAction = async (id: any) => {
+    const session = await auth();
+    const res = await sendRequest<IBackendRes<any>>({
+        url: `${process.env.NEwatchAssetsXT_PUBLIC_BACKEND_URL}/api/v1/users/${id}`,
+        method: "DELETE",
+        headers: {
+            Authorization: `Bearer ${session?.user?.access_token}`,
+        },
+    })
+
+    revalidateTag("list-users")
+    return res;
 }
